3b500eddd36a39327069841362b22159df1debcf
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-03-26 12:16:20 +0200

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import './App.css'
 
 function App() {
+  const [grid, setgrid] = useState([
+    [0],[0],[0],[0],
+    [0],[0],[0],[0],
+    [0],[0],[0],[0],
+  ])
+  function setGridValue(rowIndex: number, colIndex: number, value: number) {
+    const newGrid = [...grid];
+    newGrid[rowIndex][colIndex] = value;
+    setgrid(newGrid);
+    console.log(setgrid(newGrid));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="grid">
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+            {row.map((number, colIndex) => (
+                <div key={colIndex} className="cell">
+                    <input type="number" value={number} onChange={
+                        (e) => setGridValue(rowIndex, colIndex, parseInt(e.target.value))
+                    }/>
+                </div>
+            ))}
+        </div>
+      ))}
+        </div>
     </div>
-  );
+  )
 }
-
 export default App;
